2abac0f90061147f846b5832f397767439fb296c
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2024-11-20 14:40:09 +0700

    Initialize FastAPI application with database setup and customer routes

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = ""
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, class_=AsyncSession
+)
+Base = declarative_base()
---
+++ main.py
@@ -0,0 +1,32 @@
+from typing import List, Literal
+from uuid import uuid4
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel, Field
+from routes import customerRoute
+
+app = FastAPI(
+    title="FastAPI Shop Management",
+    description="This is a very fancy project, with auto docs for the API",
+    version="0.1.0",
+)
+# handler = Mangum(app)  # for AWS Lambda
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
+
+
+class FilterParams(BaseModel):
+    limit: int = Field(100, gt=0, le=100)
+    offset: int = Field(0, ge=0)
+    order_by: Literal["created_at", "updated_at"] = "created_at"
+    tags: list[str] = []
+
+
+app.include_router(customerRoute.router, prefix="/api/v1", tags=["Customers"])
